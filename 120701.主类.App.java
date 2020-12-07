35d2683692ea1326cb6050f4f0be016b4f4d3da6
Illusion-Yjz <74753009+Illusion-Yjz@users.noreply.github.com>
2020-12-07 15:13:30 +0800

    Create 120701.主类.App.java

---
+++ 120701.主类.App.java
@@ -0,0 +1,17 @@
+
+public class App {
+public static void main(String[] args) {
+ int array2d[][]={
+		 {1,2,3,4,5},
+		 {6,7,8,9,10},
+		 {11,12,13,14,15},
+		 {16,17,0,19,20},
+		 {21,22,23,24,25},
+		 {26,27,28,29,30},
+		 
+ };
+
+new Find().findZero(array2d);
+
+}
+}
